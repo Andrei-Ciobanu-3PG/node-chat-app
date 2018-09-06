37864de47ef25bd23cd1b535f6504517665237c3
Andrei Ciobanu <andrei.ciobanu@3pillarglobal.com>
2018-09-06 16:44:55 +0300

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.on('connect', function() {
+    console.log('connected to server');
+    socket.emit('createMessage', {
+        from:'me',
+        text:'hello server!'
+    });
+});
+
+socket.on('newMessage',function (newMessage) {
+   console.log('newMessage', JSON.stringify(newMessage, undefined, 2));
+});
+
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'server',
+        text: 'welcome',
+        createdAt: new Date().getTime()
+    });
+
+    socket.on('createMessage', (message) => {
+       console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('client disconnected');
     });
